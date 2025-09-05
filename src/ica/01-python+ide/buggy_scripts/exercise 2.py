124f3dfa5fea12ea0c6c05de12621ea6cec72cb0
gabby-bui <gbui@macalester.edu>
2025-09-05 10:35:00 -0500

    week 1_friday_in-class exercise

---
+++ src/ica/01-python+ide/buggy_scripts/exercise 2.py
@@ -0,0 +1,20 @@
+import time
+
+def demo():
+    print("Hello there!")
+    print(5+2)
+    time.sleep(1)
+    x=100
+    print(x)
+    time.sleep(1)
+    print(2*x-33)
+    time.sleep(1)
+    print(2*4)
+    time.sleep(1)
+    z=(x-(5*4))
+    print(z)
+    time.sleep(1)
+    strng = 'Hello there!'
+    print(strng)
+
+demo()
