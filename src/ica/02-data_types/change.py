6c7fbb5adf96a48dd11d8db10ea7b17ad1a6caa0
gabby-bui <gbui@macalester.edu>
2025-09-08 13:41:44 -0500

    dollar in-class exercise

--- src/ica/02-data_types/change.py
+++ src/ica/02-data_types/change.py
@@ -1,3 +1,22 @@
 price = 732
-money = input("Please enter the money amount given by the  customer in cents")
-change_dollar =   
+money = input("Please enter the money amount given by the customer in cents")
+if int(money) < price:
+    print("You don't have enough money")
+else:
+    change = int(money) - price
+    print("the change is", change)
+    dollar = change//100
+    change_left = change - dollar*100
+    quarter = change_left // 25
+    change_left = change_left - quarter*25
+    dime = change_left // 10
+    change_left = change_left - dime*10
+    nickel = change_left // 5
+    change_left = change_left - nickel*5
+    penny = change_left // 1
+    change_left = change_left - penny*1
+    print('''Dollar is:''', dollar,
+          '''Quarter is:''', quarter,
+          '''Dime is:''', dime,
+          '''Nickel is:''', nickel,
+          '''Penny is:''', penny)
