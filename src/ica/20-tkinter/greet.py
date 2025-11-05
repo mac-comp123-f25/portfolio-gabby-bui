9bbcca68c0ba2a26273bd648418b3094b78ba96a
gabby-bui <gbui@macalester.edu>
2025-11-05 10:40:03 -0600

    Update greet.py

--- src/ica/20-tkinter/greet.py
+++ src/ica/20-tkinter/greet.py
@@ -5,21 +5,34 @@ class BasicGui:
         self.mainWin = tk.Tk()
         self.guiButton = tk.Button(self.mainWin)
         self.guiButton["text"] = "Quit"
-        #quickButton = guiButton["text"] = "Quit"
         self.guiButton.grid(column=0, row=0)
 
         self.guiButton2 = tk.Button(self.mainWin)
         self.guiButton2["text"] = "Hello"
-        #quickButton2 = guiButton2["text"] = "Hello"
         self.guiButton2.grid(column=0, row=1)
 
         self.guiButton3 = tk.Button(self.mainWin)
         self.guiButton3["text"] = "Goodbye"
-        #quickButton3 = guiButton3["text"] = "Goodbye"
         self.guiButton3.grid(column=0, row=2)
+
+        self.welcome = tk.Label(self.mainWin)
+        self.welcome["text"] = "Welcome"
+        self.welcome.grid(column=1, row=1)
+
 
     def run(self):
         self.mainWin.mainloop()
 
+
+    def quit_callback(self):
+        self.mainWin.destroy()
+        self.guiButton["command"] = self.quit_callback
+
+
+    def Hellobutton(self):
+        self.welcome["text"] = "Hello"
+
+
 myGui = BasicGui()
 myGui.run()
+myGui.quit_callback()
