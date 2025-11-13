1a2b2727bc47959b2b1fc2366b75db7466d6d751
gabby-bui <gbui@macalester.edu>
2025-11-12 20:07:51 -0600

    ica

--- src/ica/15-imageTools/basics.py
+++ src/ica/15-imageTools/basics.py
@@ -5,10 +5,12 @@ pic1 = Picture("../SampleImages/mightyMidway.jpg")
 n_pixels = pic1.getWidth()*pic1.getHeight()
 print(n_pixels)
 
-last_pixel_value = img.
-
 pic2 = pic1.copy()
-
+width = pic2.getWidth()
+height = pic2.getHeight()
+corner_x = width - 1
+corner_y = height - 1
+pic2.setColor(corner_x, corner_y, "red")
 
 pic2.save("../SampleImages/mightyMidway-redCorners.jpg")
 pic2.explore()
